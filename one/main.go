0fdc7c96f98ba6f3a0762a4ce096f8fd4e3fe3f0
alekszmey1 <86463231+alekszmey1@users.noreply.github.com>
2021-12-31 09:36:54 +0700

    первое задание попытка номер 1

--- one/main.go
+++ one/main.go
@@ -9,49 +9,53 @@ import (
 
 /*Напишите HTTP-сервис, который принимает входящие соединения с JSON-данными и обрабатывает их следующим образом:
     1. Сделайте обработчик создания пользователя. У пользователя должны быть следующие поля: имя, возраст и массив друзей.
-Пользователя необходимо сохранять в мапу. Пример запроса:*/
+Пользователя необходимо сохранять в мапу. Пример запроса:
+*/
 
 type User struct {
 	Name string `json:"name"`
 	Age int `json:"age"`
-	Friends []*User
+	Friends []*User `json:"friends"`
 }
 
 func (u *User) toString() string {
-	return fmt.Sprintf("name is %s and age is %d \n", u.Name, u.Age)
+	return fmt.Sprintf("name is %s and age is %d and friends %v \n", u.Name, u.Age, u.Friends)
 }
 type service struct {
-	store map [string] *User
+	store map [int] *User
 }
 
 func main()  {
 	mux := http.NewServeMux()
-	srv := service{make(map[string]*User)}
+	srv := service{make(map[int]*User)}
 	mux.HandleFunc("/create", srv.Create)
 	mux.HandleFunc("/get", srv.GetAll	)
 	http.ListenAndServe("localhost:8080", mux)
 }
 
 func (s *service)  Create (w http.ResponseWriter, r *http.Request)  {
-	if r.Method == "POST"{
-			content, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte(err.Error()))
-				return
-			}
-			defer r.Body.Close()
-			var u User
-			if err := json.Unmarshal(content, &u); err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte(err.Error()))
-				return
-			}
-			s.store[u.Name] = &u
-			w.WriteHeader(http.StatusCreated)
-			w.Write([]byte("user was created--" + u.Name))
-			fmt.Println(&s)
-
+	var i int
+	if r.Method == "POST" {
+		content, err := ioutil.ReadAll(r.Body) // берем и считываем body (информацию с rest запроса) и заносим в переменную content (срез байтов)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError) //статус серверная ошибка
+			w.Write([]byte(err.Error()))
+			return
+		}
+		i++
+		fmt.Println(content, i ,"тый контент")
+		defer r.Body.Close()                                //отложенное закрытие считывателя
+		var u User                                          //создаем n- ого пользователя
+		if err := json.Unmarshal(content, &u); err != nil { /*благодаря Unmarshal полученная информация в байтах (content) заносится
+			в данные созданного пользователя по соответствующим полям*/
+			w.WriteHeader(http.StatusInternalServerError) //статус серверная ошибка
+			w.Write([]byte(err.Error()))
+			return
+		}
+		s.store[i] = &u // записывает в мапу с ключом = имени из данных пользователя и присваивает данному ключу полученные данные
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("user was created--" + u.Name)) // отправляем в POST информацию, что всё прошло ок
+		fmt.Println(i)
 	}
 	w.WriteHeader(http.StatusBadRequest)
 }
@@ -59,7 +63,7 @@ func (s *service)  Create (w http.ResponseWriter, r *http.Request)  {
 func (s  *service)GetAll(w http.ResponseWriter, r *http.Request)  {
 	if r.Method == "GET"{
 		response := ""
-		for _, user := range s.store {
+		for _, user := range s.store { // проходит итерацию по всей мапе и выводит информацию от сервера по позициям из мапы
 			response += user.toString()
 		}
 		w.WriteHeader(http.StatusOK)
