ca5b8c3c9cd650fce2331ec99f2f7676258381e7
alekszmey1 <86463231+alekszmey1@users.noreply.github.com>
2022-01-04 16:34:36 +0700

    первое задание попытка номер 2

--- one/main.go
+++ one/main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
 /*Напишите HTTP-сервис, который принимает входящие соединения с JSON-данными и обрабатывает их следующим образом:
     1. Сделайте обработчик создания пользователя. У пользователя должны быть следующие поля: имя, возраст и массив друзей.
 Пользователя необходимо сохранять в мапу. Пример запроса:
+Данный запрос должен возвращать ID пользователя и статус 201.
 */
 
 type User struct {
@@ -22,19 +24,19 @@ func (u *User) toString() string {
 	return fmt.Sprintf("name is %s and age is %d and friends %v \n", u.Name, u.Age, u.Friends)
 }
 type service struct {
+	i int
 	store map [int] *User
 }
 
 func main()  {
 	mux := http.NewServeMux()
-	srv := service{make(map[int]*User)}
+	srv := service{i: int(1), store: map[int]*User{}}
 	mux.HandleFunc("/create", srv.Create)
-	mux.HandleFunc("/get", srv.GetAll	)
+//	mux.HandleFunc("/get", srv.GetAll	)
 	http.ListenAndServe("localhost:8080", mux)
 }
 
 func (s *service)  Create (w http.ResponseWriter, r *http.Request)  {
-	var i int
 	if r.Method == "POST" {
 		content, err := ioutil.ReadAll(r.Body) // берем и считываем body (информацию с rest запроса) и заносим в переменную content (срез байтов)
 		if err != nil {
@@ -42,8 +44,7 @@ func (s *service)  Create (w http.ResponseWriter, r *http.Request)  {
 			w.Write([]byte(err.Error()))
 			return
 		}
-		i++
-		fmt.Println(content, i ,"тый контент")
+		//fmt.Println(content,s.i, "тый контент")
 		defer r.Body.Close()                                //отложенное закрытие считывателя
 		var u User                                          //создаем n- ого пользователя
 		if err := json.Unmarshal(content, &u); err != nil { /*благодаря Unmarshal полученная информация в байтах (content) заносится
@@ -52,14 +53,17 @@ func (s *service)  Create (w http.ResponseWriter, r *http.Request)  {
 			w.Write([]byte(err.Error()))
 			return
 		}
-		s.store[i] = &u // записывает в мапу с ключом = имени из данных пользователя и присваивает данному ключу полученные данные
+		s.store[s.i] = &u // записывает в мапу с ключом = имени из данных пользователя и присваивает данному ключу полученные данные
+
 		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte("user was created--" + u.Name)) // отправляем в POST информацию, что всё прошло ок
-		fmt.Println(i)
+		w.Write([]byte("пользователь " + u.Name + " зарегистрирован под номером " + strconv.Itoa(s.i))) // отправляем в POST информацию, что всё прошло ок
+		//fmt.Println(u)
+		s.i++
+		//fmt.Println(s)
 	}
 	w.WriteHeader(http.StatusBadRequest)
 }
-
+/*
 func (s  *service)GetAll(w http.ResponseWriter, r *http.Request)  {
 	if r.Method == "GET"{
 		response := ""
@@ -71,4 +75,4 @@ func (s  *service)GetAll(w http.ResponseWriter, r *http.Request)  {
 		return
 	}
 	w.WriteHeader(http.StatusBadRequest)
-}
+}*/
